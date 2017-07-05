9eaa442d7ce50c381ae8e092e413bbd206b23441
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-05 10:50:04 +0530

    Create Factorialno.java

---
+++ Factorialno.java
@@ -0,0 +1,20 @@
+# factorial
+import java.io.*;
+import java.util.*;
+public class Factorialno
+{
+  public static void main(String args[])
+  {
+  Scanner sc=new Scanner (System.in);
+  int a;
+    System.out.println("Enter the num");  
+  a=sc.nextInt();
+  int fact=1;
+    for(int i=1;i<=a;i++)
+  {
+      fact=fact*i;
+      }
+      System.out.println(fact);
+      }
+      }  
+      
